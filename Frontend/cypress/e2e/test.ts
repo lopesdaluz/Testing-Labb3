30e4c6fea9cf369d69f1d662348374c97bab3c50
lopesdaluz <tlopesdaluz@gmail.com>
2023-12-13 15:18:05 +0100

    BDD-rullgardin test

--- Frontend/cypress/e2e/test.ts
+++ Frontend/cypress/e2e/test.ts
@@ -9,4 +9,11 @@ Before(() => {
   cy.visit("http://localhost:5173/");
 });
 
-Given("Jag är på hemsidan och den visar en input fält ", () => {});
+Given(
+  "Jag är på hemsidan och ser en rullgardinsmeny där man kan välja gender",
+  () => {}
+);
+
+When("Jag klickar på rullgardinmenyn", () => {});
+
+Then("Menyn visar alternativ ", () => {});
