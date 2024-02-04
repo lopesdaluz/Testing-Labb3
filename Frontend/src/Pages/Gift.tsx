b52f7e01c6099ff4f421d85662a24b67d39a14a8
lopesdaluz <tlopesdaluz@gmail.com>
2024-02-04 10:08:44 +0100

    design for gift component

--- Frontend/src/Pages/Gift.tsx
+++ Frontend/src/Pages/Gift.tsx
@@ -9,26 +9,48 @@ interface GiftProps {
 }
 
 const Gift: React.FC<GiftProps> = ({ children }) => {
-  const { cardName } = useParams<{ cardName?: CardId }>();
+  const { cardName } = useParams<{ cardName: CardId }>();
   console.log("Card Name:", cardName);
   const [productlist, setproductlist] = useState([]);
+  const [sortedProducts, setSortedProducts] = useState([]);
 
   useEffect(() => {
     getProduct();
-    console.log(productlist);
-  }, []);
+  }, [cardName]);
+
+  type product = {
+    id: number;
+    name: string;
+    category: string;
+    price: string;
+    img_url: string;
+  };
 
   type ProductResponse = {
-    data: data;
+    data: product[];
   };
 
   async function getProduct(): Promise<ProductResponse> {
     try {
-      let products = await fetch("http://localhost:3000/products");
-      setproductlist(products);
-      console.log(products);
+      let response = await fetch("http://localhost:3000/products");
+      let data = await response.json();
+      setproductlist(data);
+
+      //göra  en filtrering här med variabel 'SortedProducts'
+
+      const filteredProducts = data.filter(
+        (product) => product.category === cardName
+      );
+      setSortedProducts(filteredProducts);
+      console.log("The product list", productlist);
+      console.log("The card names:", cardName);
+      console.log("The filtered Products:", sortedProducts);
+
+      console.log(data);
+      return { data };
     } catch (error) {
       console.log(error);
+      throw error;
     }
   }
 
@@ -48,6 +70,16 @@ const Gift: React.FC<GiftProps> = ({ children }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <div className="product-list">
+        {sortedProducts.map((product) => (
+          <div key={product.id} className="product-item">
+            <img src={product.img_url} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>Category: {product.category}</p>
+            <p>Price: {product.price}</p>
+          </div>
+        ))}
+      </div>
       {children}
     </div>
   );
